8f712db7447153cff79b65757ac8e98d904e3938
Elieacy <chenjiehui9029@163.com>
2022-12-11 18:14:50 +0800

    master commit1

---
+++ src/main/java/com/gec/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.gec.test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("helloWorld!1");
+        System.out.println("helloWorld!2");
+        System.out.println("helloWorld!3");
+    }
+
+}
